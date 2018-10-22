ac8f8cbf9dca33bbee0a6ad41b8e9bdfab12879d
vitorino87 <brother_87_22@hotmail.com>
2018-10-22 15:21:23 -0200

    melhorias realizadas em 22-10-18

--- src/servidor_cripto_udp_atv8_seg_redes/UDPServer.java
+++ src/servidor_cripto_udp_atv8_seg_redes/UDPServer.java
@@ -4,27 +4,39 @@ package servidor_cripto_udp_atv8_seg_redes;
 import java.io.*;
 import java.net.*;
 
-class UDPServer
-{
-   //public static void main(String args[]) throws Exception
-    public void iniciarComunicacao() throws Exception
-      {
-         DatagramSocket serverSocket = new DatagramSocket(9876);
-            byte[] receiveData = new byte[1024];
-            byte[] sendData = new byte[1024];
-            while(true)
-               {
-                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                  System.out.println("Servidor aguardando..." );
-                  serverSocket.receive(receivePacket);
-                  String sentence = new String( receivePacket.getData());
-                  System.out.println("Mensagem recebida: " + sentence);
-                  InetAddress IPAddress = receivePacket.getAddress();
-                  int port = receivePacket.getPort();
-                  String capitalizedSentence = sentence.toUpperCase();
-                  sendData = capitalizedSentence.getBytes();
-                  DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                  //serverSocket.send(sendPacket);
-               }
-      }
+class UDPServer {
+    //public static void main(String args[]) throws Exception
+
+    public void iniciarComunicacao() throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+        String sentence;
+        
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            System.out.println("******************************************************");
+            System.out.println("Servidor aguardando...");
+            serverSocket.receive(receivePacket);
+            sentence = new String(receivePacket.getData());
+            sentence = sentence.trim();
+            System.out.println("Mensagem recebida: " + sentence);
+            System.out.println("Para descriptografar a mensagem, digite a senha:");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String senha = br.readLine();
+            StringEncrypter c = new StringEncrypter(senha);
+            sentence = c.decrypt(sentence);
+            System.out.println("Mensagem descriptografada: "+sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+            receivePacket = null;
+            receiveData = null;
+            receiveData = new byte[1024];
+            System.out.println("******************************************************");
+        }
+    }
 }
